9d2291f2fedeb82a375696e64c8b9a5cc1fbdf0c
Anders Engan <anders-eng@hotmail.com>
2019-01-13 13:57:45 +0100

    Including dynamics echarts for ore exposures

--- App/App/Controllers/RunController.cs
+++ App/App/Controllers/RunController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
 using System.IO;
+using System.Data;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting;
@@ -34,14 +35,16 @@ namespace App.Controllers
             // With Docker enabled hosting environment path defaults to C:\ and environment to "Development"
 
             //string root = _config.Value.OrePath;
-            string root = "C:\\developer\\git\\Engine";
+            string root = "C:\\developer\\git\\ore\\Engine";
             //string root = _env.ContentRootPath.ToString();
 
             string runFile = string.Format("{0}\\{1}", root, "Examples\\Example_1\\run.py");
+            //string runFile = string.Format("{0}\\{1}", root, "~/developer/git/ore/Engine/Examples/Example_1/run.py");
 
             ProcessStartInfo start = new ProcessStartInfo();
             start.WorkingDirectory = string.Format("{0}\\{1}", root, "Examples\\Example_1");
-            start.FileName = "python.exe"; // Assumes path to python.exe in PATH-variable
+            //start.WorkingDirectory = string.Format("{0}\\{1}", root, "~/developer/git/ore/Engine/Examples/Example_1");
+            start.FileName = "python"; // Assumes path to python.exe in PATH-variable
             start.Arguments = string.Format("{0} {1}", runFile, "");
             start.UseShellExecute = false;
             start.RedirectStandardOutput = true;            
@@ -64,6 +67,48 @@ namespace App.Controllers
             console.Text = str; 
 
             return View(console);
-        }        
+        }
+
+        public ActionResult LoadExposure()
+        {            
+            //string root = _config.Value.OrePath;
+            string root = "C:\\developer\\git\\ore\\Engine";
+            //string root = _env.ContentRootPath.ToString();            
+
+            string csv_name = string.Format("{0}\\{1}", root, "Examples\\Example_1\\Output\\exposure_trade_Swap_20y.csv");
+            int time_colidx = 2;
+            int val_colidx = 3;       
+
+            List<double> time = GetData(csv_name, time_colidx);
+            List<double> values = GetData(csv_name, val_colidx);
+
+            Exposure model = new Exposure()
+            {
+                Time = time,
+                Values = values
+            };
+
+            return View(model);
+        }
+
+        private List<double> GetData(string fileName, int colidx, int offset = 1)
+        {
+            List<double> data = new List<double>();
+            List<object> temp = new List<object>();
+
+            FileStream fileStream = new FileStream(fileName, FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (colidx < line.Split(',').Length) temp.Add(line.Split(',')[colidx]);
+                }
+            }
+
+            data = temp.Skip(offset).Select(d => Convert.ToDouble(d)).ToList();
+           
+            return data;
+        }
     }
 }
---
+++ App/App/Models/Exposure.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Models
+{
+    public class Exposure
+    {
+        public List<double> Time { get; set; }
+        public List<double> Values { get; set; }
+    }
+}

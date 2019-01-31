daca0d02dfcee5504a2975272682093d1134a770
Mikael Radomski <mikael.radomski@gmail.com>
2019-01-31 20:47:00 +0100

    xml object model, in process

---
+++ App/App/Helpers/Extensions/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Dynamic;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace App.Helpers.Extensions
+{
+    public static class Extensions
+    {
+        public static ExpandoObject ToExpando(this object anonymousObject)
+        {
+            IDictionary<string, object> anonymousDictionary = HtmlHelper.AnonymousObjectToHtmlAttributes(anonymousObject);
+            IDictionary<string, object> expando = new ExpandoObject();
+            foreach (var item in anonymousDictionary)
+                expando.Add(item);
+            return (ExpandoObject)expando;
+        }
+
+    }
+}
